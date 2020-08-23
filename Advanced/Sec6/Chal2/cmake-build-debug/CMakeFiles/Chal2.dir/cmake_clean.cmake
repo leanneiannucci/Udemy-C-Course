61d5dd8b030e8ce3742ec5d9631bad73b28dd59a
Leanne <leanneiannucci@gmail.com>
2020-08-22 21:44:03 -0500

    addded advanced sec6 challenge2 files

---
+++ Advanced/Sec6/Chal2/cmake-build-debug/CMakeFiles/Chal2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chal2.dir/main.c.o"
+  "Chal2"
+  "Chal2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chal2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
