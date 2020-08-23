fbd6c623bc5e0fbf59ad43010ce798d3d6dffb53
Leanne <leanneiannucci@gmail.com>
2020-08-22 22:00:11 -0500

    finished section 6 challenge 2

--- Advanced/Sec6/Chal2/main.c
+++ Advanced/Sec6/Chal2/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct s{
+    int arraySize;
+    int array[];
+};
 
 int main() {
-    printf("Hello, World!\n");
+
+    struct s *ptr;
+
+    printf("How long do you want the FAM to be?");
+    scanf("%d", &(ptr->arraySize));
+    int tmp = ptr->arraySize;
+    ptr = malloc(sizeof(struct s) + (ptr->arraySize * (sizeof(int))));
+    ptr->arraySize = tmp;
+
+
+    for (int i = 0; i<(ptr->arraySize); i++){
+        printf("Add element %d to FAM.\n", i+1);
+        scanf("%d", &ptr->array[i]);
+    }
+
+    for (int i = 0; i<(ptr->arraySize); i++){
+        printf("Element %d: %d\n", i+1, ptr->array[i]);
+    }
+
+
+
+
+
+
+
     return 0;
 }
