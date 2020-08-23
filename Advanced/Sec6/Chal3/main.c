b3f49e5f7458d8a345d914725d1925e2db919572
Leanne <leanneiannucci@gmail.com>
2020-08-22 22:16:34 -0500

    added comments to main... skipping chal3 for now. might come back later.

--- Advanced/Sec6/Chal3/main.c
+++ Advanced/Sec6/Chal3/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <complex.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // i didnt feel like i wanted to do this one nor did it really help me all that much. might come back after getting
+    // further into MC
     return 0;
 }
