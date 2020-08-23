0146b25b0207cec2928a26afb3f28da85b591bc5
Leanne <leanneiannucci@gmail.com>
2020-08-22 21:15:16 -0500

    removed old git folder from beg sec5 chal2

---
+++ Beginner/Sec5/Chal2/cmake-build-debug/CMakeFiles/Chal2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Chal2.
+# This may be replaced when dependencies are built.
---
+++ Beginner/Sec5/Chal2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    enum Company {GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT};
+
+    enum Company var1=XEROX, var2=GOOGLE, var3=EBAY;
+
+    printf("%i\n", var1);
+    printf("%i\n", var2);
+    printf("%i\n", var3);
+
+
+    // trying git things
+
+    return 0;
+}
