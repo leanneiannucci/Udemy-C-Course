e06fec94deeabe27fd9e7a550f01fcc47a7d5bd5
Leanne <leanneiannucci@gmail.com>
2020-08-22 21:37:12 -0500

    finished advanced sec6 chal1

--- Advanced/Sec6/Chal1/main.c
+++ Advanced/Sec6/Chal1/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int main() {
-    printf("Hello, World!\n");
+    int els = 0;
+    int sum = 0;
+
+    printf("How long do you want the array to be?\n");
+    scanf("%d", &els);
+
+    int arr[els];
+
+    for (int i =0; i < els; i++){
+        printf("Please enter Element #%d:\n", i+1);
+        scanf("%d",&arr[i]);
+        sum = sum+arr[i];
+    }
+
+    printf("The sum of all these elements is %d", sum);
+
     return 0;
 }
