6ecdb3980336794bdf78359cf9104f8221b16e8d
Leanne <leanneiannucci@gmail.com>
2020-08-31 21:44:35 -0500

    finished chal 1

--- Advanced/Sec8/Chal1/main.c
+++ Advanced/Sec8/Chal1/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <math.h>
+
+int bin2dec(long long bin);
 
 int main() {
-    printf("Hello, World!\n");
+    long long n;
+    printf("Enter a binary number: ");
+    scanf("%lld", &n);
+    printf("In decimal: %d\n", bin2dec(n));
     return 0;
+}
+
+
+int bin2dec(long long bin) {
+    int conv, count = 0;
+    while(bin) {
+
+        if (bin % 10) {
+            conv = conv + (int)(pow(2,count));
+        }
+        bin = (long long)(bin/10);
+        count++;
+    }
+
+    return conv;
+
 }
