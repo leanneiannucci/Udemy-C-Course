5d0694a6c6f0314358aa34f0d6d6eef64ff1608a
Leanne <leanneiannucci@gmail.com>
2020-08-31 21:54:33 -0500

    finished adv sec8 challenge2

--- Advanced/Sec8/Chal2/main.c
+++ Advanced/Sec8/Chal2/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <math.h>
+
+
+long long dec2bin(int n);
 
 int main() {
-    printf("Hello, World!\n");
+    int n;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    printf("In binary: %lld\n", dec2bin(n));
     return 0;
 }
+
+long long dec2bin(int n){
+    int count = 0;
+    long long conv;
+    while(n) {
+        if (n % 2) {
+            conv = conv + (long long)(pow(10,count));
+        }
+        n = (int)(n/2);
+        count++;
+    }
+
+    return conv;
+
+
+
+}
+
